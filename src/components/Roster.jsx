cbe9aec365f9e49615de7bbdaac719cba0731913
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-01-03 04:00:03 -0800

    [PROJ-83] Roster Online (#12)

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -2,10 +2,26 @@ import { useEvent } from '../context/EventProvider';
 import "./Main.css";
 
 const Roster = (props) => {
+  let members = props.members.slice();
+  if (props.user.geofenceslength) {
+    let me = members.find(member => member.id === props.user.userId)
+    me.place = 'home'
+  }
   const { familyList } = useEvent();
+  if (familyList) {
+    let activeList = familyList.members;
+    for (const key of Object.keys(activeList)) {
+      let member = members.find(member=>member.id === key);
+      if (!member) {
+        continue;
+      }
+      member.place = 'home';
+    }
+  }
+
   return(
     <div>
-      {props.members.map(member => <button key={member.id} className="homeList">{member.name}</button>)}
+      {members.map(member => <button key={member.id} className={member.place ? `${member.place} homeList` : "homeList"}>{member.name}</button>)}
     </div>
   );
 }
