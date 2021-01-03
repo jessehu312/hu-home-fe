d7262009218055153532fa1129a8f8f00b5486b5
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-01-03 00:30:27 -0800

    [PROJ 77] Created main page template (#10)
    * [PROJ-77] Added main page template
    
    * Fixed UserProvider

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,11 @@
 import { useConfig } from '../context/ConfigProvider';
 import { UserProvider, useCurrentUser } from '../context/UserProvider';
 import { EventProvider, useEvent } from '../context/EventProvider';
+import "./Main.css";
+import MainButton from './MainButton.jsx';
+import AccIcon from '../AccountIcon.png';
+import ConIcon from '../ContactIcon.png';
+import SetIcon from '../SettingsIcon.png';
 
 const UserData = () => {
   const { currentUser, location, locationDetails, debug } = useCurrentUser();
@@ -13,14 +18,20 @@ const UserData = () => {
 const Main = ({ user, providerId }) => {
   const { config: { firebaseClient } } = useConfig();
   return (
-    <div className="container">
-      <button onClick={async () => { await firebaseClient.auth().signOut(); }}>Sign out</button>
-      <pre style={{ overflow: "auto" }}>{JSON.stringify({ user, providerId }, null, 2)}</pre>
-      <UserProvider>
-        <EventProvider>
-          <UserData/>
-        </EventProvider>
-      </UserProvider>
+    <div className="container"> 
+      <div className="navheaderMain">
+        <h1>Currently Home:</h1>
+      </div>
+      <MainButton icon={AccIcon}>
+        Manage Addresses
+      </MainButton>
+      <MainButton icon={ConIcon}>
+        Manage Contacts
+      </MainButton>
+      <MainButton icon={SetIcon}>
+        Privacy &amp; Settings
+      </MainButton>
+      <button onClick={async () => { await firebaseClient.auth().signOut(); }} className="logout">Sign out</button>
     </div>
   )
 };
